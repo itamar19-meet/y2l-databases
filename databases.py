4c8cf086c9911d24ee47bcbc9a024d6f55760556
aboud20-meet <aboud20@meet.mit.edu>
2019-02-24 18:45:35 +0200

    finished

--- databases.py
+++ databases.py
@@ -10,16 +10,29 @@ session = DBSession()
 
 # Write your functions to interact with the database here :
 
-def create_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
+def add_person(favorite_food,gender,height,age,name):
+  person_object  = persons(favorite_food,gender,height,age,name)
+  session.add(person_object)
+  session.commit()
+  return person_object
   pass
 
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
+def update_product(person_id,new_fav_food, new_gender):
+  person_object1=session.query(persons).filter_by(person_id=person_id).first()
+  person_object1.favorite_food = new_fav_food;
+  person_object1.gender = new_gender;
+  session.commit()
+  return person_object
   pass
 
-def delete_product(id):
-  pass
+def delete_product(person_id):
+    session.query(persons).filter(person.person_id).delete()
+    session.commit()
+    pass
 
-def get_product(id):
-  pass
+def get_product():
+    to_get =session.query(persons).first()
+    print(to_get.favorite_food+to_get.gender+to_get.height+to_get.age+to_get.name)
+    return to_get
+def get_all():
+    return session.query(persons).all()
--- model.py
+++ model.py
@@ -6,6 +6,12 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 # Write your classes here :
-class Product(Base):
-    # TODO: complete this class
+class person(Base):
+    __tablename__ = 'persons'
+	person_id = Column(Integer ,  primary_key = True , autoincrement=True)
+	name = Column(String)
+	age = Column(Integer)
+	height = Column(Integer)
+	gender = Column(String)
+	favorite_food = Column(String)
     pass
